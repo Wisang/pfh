44162f256921e632fbac115c7e190f0e4b845bfa
wisang <wseom7@gmail.com>
2023-12-22 22:40:01 +0900

    function added

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-ip = input("Enter your ip: ")
-print("Your ip is: " + ip)
+words = ["hacker", "bounty", "hunter"]
+
+def is_word_in_string(string):
+    for word in words:
+        if word in string:
+            return "It exists!"
+    return "It does not exist!"
+
+is_word_in_string("hacker")
+
+is_word_in_string("bounty")
+
+is_word_in_string("hunter")
+
+is_word_in_string("white-hacker")
